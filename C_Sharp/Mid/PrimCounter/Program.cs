97d92d7b58501265afd3421554e6c101f84c98ef
StepanianPetros <164545909+StepanianPetros@users.noreply.github.com>
2025-05-21 13:10:25 +0400

    Add files via upload

---
+++ C_Sharp/Mid/PrimCounter/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Channels;
+
+public class Solution {
+    public int CountPrimes(int n) {
+        if (n <= 2)
+        {
+            return 0;
+        }
+
+        var count = n / 2;
+        var isPrimesIn = new bool[n];
+
+        for (int i = 3; i * i < n; i += 2)
+        {
+            if (!isPrimesIn[i])
+            {
+                for (int j = i * i; j < n; j += 2 * i)
+                {
+                    if (!isPrimesIn[j])
+                    {
+                        count--;
+                        isPrimesIn[j] = true;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+}
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            string? input = Console.ReadLine();
+            int num;
+            bool success = int.TryParse(input, out num);
+            if (success)
+            {
+                Solution solution = new Solution();
+                Console.WriteLine("Answer is: " + solution.CountPrimes(num));
+            }
+            else
+            {
+                Console.WriteLine("Input error");
+            }
+           
+        }
+    }
+}
+
+
+
+
