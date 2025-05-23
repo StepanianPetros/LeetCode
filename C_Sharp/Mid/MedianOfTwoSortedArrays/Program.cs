085277d305853c01b2fad053f66ee63457f66751
StepanianPetros <164545909+StepanianPetros@users.noreply.github.com>
2025-05-23 22:06:16 +0400

    solve

---
+++ C_Sharp/Mid/MedianOfTwoSortedArrays/Program.cs
@@ -0,0 +1,47 @@
+﻿public class Solution
+{
+    public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+    {
+        int[] sorted = new int[nums1.Length + nums2.Length];
+        int indexOne = 0, indexTwo = 0, indexSorted = 0;
+
+        while (indexOne < nums1.Length && indexTwo < nums2.Length)
+        {
+            if (nums1[indexOne] <= nums2[indexTwo])
+            {
+                sorted[indexSorted++] = nums1[indexOne++];
+            }
+            else
+            {
+                sorted[indexSorted++] = nums2[indexTwo++];
+            }
+        }
+
+        while (indexOne < nums1.Length)
+        {
+            sorted[indexSorted++] = nums1[indexOne++];
+        }
+
+        while (indexTwo < nums2.Length)
+        {
+            sorted[indexSorted++] = nums2[indexTwo++];
+        }
+
+        int totalLength = sorted.Length;
+        if (totalLength % 2 == 0)
+        {
+            return (sorted[totalLength / 2 - 1] + sorted[totalLength / 2]) / 2.0;
+        }
+
+        return sorted[totalLength / 2];
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Solution solution = new Solution();
+        Console.WriteLine(solution.FindMedianSortedArrays([1, 2, 5], [3, 4, 17]));
+    }
+}
