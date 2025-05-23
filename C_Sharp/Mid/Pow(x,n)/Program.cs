8df8a9e2681764521822d3744e886300c18d2da8
StepanianPetros <164545909+StepanianPetros@users.noreply.github.com>
2025-05-23 21:22:36 +0400

    Add files via upload

---
+++ C_Sharp/Mid/Pow(x,n)/Program.cs
@@ -0,0 +1,39 @@
+﻿
+public class Solution 
+{
+    public double MyPow(double x, int n)
+    {
+        long N = n;
+        if (N < 0) 
+        {
+            x = 1 / x;
+            N = -N;
+        }
+
+        double result = 1.0;
+        while (N > 0)
+        {
+            if (N % 2 == 1)
+            {
+                result *= x;
+            }
+            x *= x;
+            N /= 2;
+        }
+
+        return result;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Solution solution = new Solution();
+        Console.WriteLine(solution.MyPow(9.0000, -2));
+    }
+}
+
+
+
+
